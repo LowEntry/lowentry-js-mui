6931ee1fe1f8ee3fcd022a48f1f8ed6fa147c30f
Patrick Vane <patrick_vane@hotmail.com>
2025-07-08 21:09:39 +0200

    Made it possible to output typescript definition files now, although it doesn't do so yet (issues with how the components are built and exported separately). Also changed the testing framework to vitest.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,4 @@
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
@@ -6,6 +6,7 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import cssnano from 'cssnano';
 import autoprefixer from 'autoprefixer';
 import url from '@rollup/plugin-url';
+import execute from 'rollup-plugin-execute';
 
 
 import path from 'path';
@@ -17,6 +18,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
+fse.emptyDirSync('build');
 fse.emptyDirSync('dist');
 fse.copySync('src', 'dist/src');
 
@@ -68,13 +70,13 @@ export default {
 		resolve(),
 		commonjs(),
 		babel({
-			runtimeHelpers:true,
-			exclude:       'node_modules/**',
-			presets:       [
+			babelHelpers:'runtime',
+			exclude:     'node_modules/**',
+			presets:     [
 				'@babel/preset-env',
 				'@babel/preset-react',
 			],
-			plugins:       [
+			plugins:     [
 				'@babel/plugin-transform-runtime',
 			],
 		}),
@@ -86,5 +88,6 @@ export default {
 			inject: true,
 		}),
 		url(),
+		//execute(['npx tsc', 'npx api-extractor run'], true),
 	],
 };

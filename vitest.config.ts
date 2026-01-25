bb283c51511b31e107a55d607479e6115363fef9
Patrick Vane <patrick_vane@hotmail.com>
2026-01-25 23:54:25 +0100

    Finalized the migration.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,19 +1,20 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
 	test: {
+		globals: true,
 		environment: 'jsdom',
-		include: ['./tests/**/*.test.tsx', './tests/**/*.test.ts'],
 		coverage: {
 			provider: 'v8',
 			reporter: ['text', 'json', 'html'],
-			exclude: [
-				'node_modules/**',
-				'dist/**',
-				'tests/**',
-				'vitest.config.ts',
-			],
+			include: ['src/**/*.{ts,tsx}'],
+			exclude: ['src/**/*.d.ts'],
+			thresholds: {
+				lines: 100,
+				functions: 100,
+				branches: 100,
+				statements: 100,
+			},
 		},
 	},
 });
